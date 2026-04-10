2d283d01f0743d0588adc6375ff6a038ab60636d
sahilchauhan123 <sahilchauhanc123@gmail.com>
2026-04-10 09:33:43 +0530

    replace base url with .env

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://[IP_ADDRESS]/api/v1';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api/v1';
 
 export async function fetchProducts(query: string) {
     try {
